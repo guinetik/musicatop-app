e1a7c9fcac0d44a981cec71a447637d76656c899
João Guilherme <guinetik@gmail.com>
2015-02-20 10:49:12 -0300

    integrando pagina do cd com backend

--- app/www/js/ctrls/CD.js
+++ app/www/js/ctrls/CD.js
@@ -2,6 +2,24 @@
  * Created by guinetik on 2/20/15.
  */
 angular.module('mt.controllers').controller('CD', CD);
-function CD($scope, $rootScope) {
-
+function CD($scope, $rootScope, api, $stateParams, $timeout, toastr) {
+    $scope.cd = {};
+    $scope.cd.id = $stateParams.id;
+    $scope.$on('$ionicView.beforeEnter', function (event) {
+        $scope.dataLoaded = false;
+    });
+    $scope.$on('$ionicView.enter', function (event) {
+        $timeout(function () {
+            api.getCD($scope.cd.id, function (result) {
+                if (result.status == 200) {
+                    $scope.dataLoaded = true;
+                    $scope.cd = result.cd;
+                }
+            });
+        });
+    });
+    $scope.addToPlaylist = function (musica) {
+        musica.cd = $scope.cd;
+        $rootScope.$emit("add-to-playlist", musica);
+    };
 }
